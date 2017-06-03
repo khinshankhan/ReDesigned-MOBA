0952fa08ca5e2d8617fe892f22d85a0942e9c202
Khinshan Khan <kkhan@stuy.edu>
2017-06-03 18:38:50 -0400

    hexagonal map started. now for path finding

---
+++ main/Map.java
@@ -0,0 +1,55 @@
+public class Map{
+    Mnode[][] map;
+    int rows, cols;
+
+    public Map(int rows, int cols){
+	this.rows=rows;
+	this.cols=cols;
+	map= new Mnode[rows][cols];
+	for(int i=0; i< rows; i++){
+	    for(int j=0; j< cols; j++){
+		map[i][j]= new Mnode(j/2,i, false);
+	    }
+	}
+	for(int i=0, k=0; i< rows; i++, k++){
+	    for(int j=0; j< cols; j+=2){
+		if(k%2==0 && j==0){
+		    j++;
+		}
+		map[i][j].walkable= true;
+	    }
+	}
+    }
+
+    public void print(){
+	for(int i=0; i< rows; i++){
+	    for(int j=0; j< cols; j++){
+		Mnode here= map[i][j];
+		if(here.walkable==true){
+		    System.out.print("("+here.x+","+here.y+")");
+		}else if(i%2==0 && j==0){
+		    System.out.print("  ");
+		}else{
+		    System.out.print("");
+		}
+	    }
+	    System.out.println();
+	}
+	for(int i=0; i< rows; i++){
+	    for(int j=0; j< cols; j++){
+		Mnode here= map[i][j];
+		if(here.walkable==true){
+		    System.out.print(1);
+		}else{
+		    System.out.print(" ");
+		}
+	    }
+	    System.out.println();
+	}
+    }
+    
+    public static void main(String args[]){
+	Map a= new Map(10,10);
+	a.print();
+    }
+}
--- main/Mnode.java
+++ main/Mnode.java
@@ -2,10 +2,13 @@ public class Mnode{
 
     public int x;
     public int y;
+    public int z;
     public boolean walkable;
 
-    public Mnode(int dx, int dy){
+    public Mnode(int dx, int dy, boolean clear){
 	x = dx;
 	y = dy;
+	z = x *-1 -y;
+	walkable = clear;
     }
 }
